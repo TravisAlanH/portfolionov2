c470c94a1bf58ac234de988211e29f0637a3a544
TravisAlanH <travis.Heidelberger@trojans.dsu.edu>
2022-12-15 17:25:33 -0600

    Clean up for build

--- src/Components/PageHeader.js
+++ src/Components/PageHeader.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link } from "react-router-dom";
+// import { Link } from "react-router-dom";
 import { FaAngleDown } from "react-icons/fa";
 import data from "../Resume";
 
